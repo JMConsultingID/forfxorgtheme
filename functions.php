a34655168de7cf970747a1dea84a03068a61e580
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-12-05 14:08:31 +0800

    Initial commit
    Initial commit

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * https://developers.elementor.com/docs/hello-elementor-theme/
+ *
+ * @package HelloTheme
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+define('HELLO_THEME_VERSION', '2.1.9');
+
+/**
+ * Load hello theme scripts & styles.
+ *
+ * @return void
+ */
+require_once get_stylesheet_directory() . '/inc/functions/forfx-theme-functions.php';
+
+function hello_theme_scripts_styles()
+{
+    wp_enqueue_style('hello-theme-style', get_stylesheet_directory_uri() . '/style.css', [], HELLO_THEME_VERSION);
+    wp_enqueue_style('hello-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/forfx-theme.css', [], HELLO_THEME_VERSION);
+    wp_enqueue_script('hello-theme-custom-script', get_stylesheet_directory_uri() . '/assets/js/forfx-theme.js', [], HELLO_THEME_VERSION, true);
+}
+add_action('wp_enqueue_scripts', 'hello_theme_scripts_styles', 20);
